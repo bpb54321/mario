92a49b5cc43b2334cdb05670d8e77d720e4dfb25
Brian Blosser <bpb54321@gmail.com>
2015-07-13 14:42:24 -0500

    Introduced Switch statement for printing to console vs file.

--- Mario.java
+++ Mario.java
@@ -39,10 +39,30 @@ public class Mario
                //Since it passed verification, input is the height of the pyramid. 
                int height=Integer.valueOf(currentInputString);
                
+               //Ask the user if they want the pyramid printed to the screen or to a file
+               System.out.println("Enter '1' if you would like the pyramid to print to the screen, " +
+                       "'2' if you would like it to print to a file.\n");
+               currentInputString=scanner.nextLine();
+               //System.out.println("The number you have chosen is " + currentInputString + "\n");
+               //Switch block
+               switch (currentInputString) {
+                   case "1":
+                       System.out.println("The number you have chosen is 1. \n");
+                       break;
+                   case "2":
+                       System.out.println("The number you have chosen is 2. \n");
+                       break;
+                   default:
+                       System.out.println("You have entered an invalid number. \n");
+                       break;
+               }
+
+
+
                /////Block for printing the pyramid
                int numOfLeftSpaces=height-1;
                int numOfHashes=2;
-               String outputString="";
+               String outputString;
                for (int i=0; i<height; i++ ) {
                    outputString="";
                    
